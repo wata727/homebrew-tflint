3e0195ad0528f4dfe1159204b0e6f4c6440c887b
Steve Huff <shuff@vecna.org>
2017-03-20 14:03:34 -0400

    Initial commit

---
+++ Formula/tflint.rb
@@ -0,0 +1,26 @@
+class Tflint < Formula
+  desc "Terraform linter"
+  homepage "https://github.com/wata727/tflint"
+  url "https://github.com/wata727/tflint/archive/v0.3.1.tar.gz"
+  sha256 "63b58673b7a8535138fdd03c5fc40115e87f229aa14ed25ad7bf922c80676d74"
+
+  depends_on "glide" => :build
+  depends_on "go" => :build
+  depends_on "terraform"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home"
+    (buildpath/"src/github.com/wata727/tflint").install buildpath.children
+    cd "src/github.com/wata727/tflint" do
+      system "glide", "install"
+      system "go", "build", "-o", "tflint"
+
+      bin.install "tflint"
+    end
+  end
+
+  test do
+    system "#{bin}/tflint", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# homebrew-tflint
+
+[Homebrew](https://brew.sh) formula for [tflint](https://github.com/wata727/tflint).
+
+## Usage
+
+```sh
+$ brew tap hakamadare/tflint
+$ brew install tflint
+```
