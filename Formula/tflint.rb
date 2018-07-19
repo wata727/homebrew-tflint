8518d2fb0da075ce5ed82d885f3be2385039fa14
wata_mac <watassbass@gmail.com>
2018-07-19 23:08:27 +0900

    Bump up version to v0.7.1

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -1,18 +1,17 @@
 class Tflint < Formula
   desc "Terraform linter"
   homepage "https://github.com/wata727/tflint"
-  url "https://github.com/wata727/tflint/archive/v0.7.0.tar.gz"
-  sha256 "5a5bc319d7622a5091032295571e4c42c5ad16a4ee41eb2760cf048441e59856"
+  url "https://github.com/wata727/tflint/archive/v0.7.1.tar.gz"
+  sha256 "c9e8fbe93bf3fdcb5653c3ccc18b2bf135bd064014cb5015d191bd8b4843cf09"
 
-  depends_on "glide" => :build
+  depends_on "dep" => :build
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home"
     (buildpath/"src/github.com/wata727/tflint").install buildpath.children
     cd "src/github.com/wata727/tflint" do
-      system "glide", "install"
+      system "dep", "ensure", "-vendor-only"
       system "go", "build", "-o", "tflint"
 
       bin.install "tflint"
