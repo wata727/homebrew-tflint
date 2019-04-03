ea244e1c808ace8dcfc0464a7c9ee44c6521e7b4
wata_mac <watassbass@gmail.com>
2019-04-04 01:49:46 +0900

    Bump up version to v0.7.5

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -1,8 +1,8 @@
 class Tflint < Formula
   desc "Terraform linter"
   homepage "https://github.com/wata727/tflint"
-  url "https://github.com/wata727/tflint/archive/v0.7.4.tar.gz"
-  sha256 "1b9f69e44fc69dead8f3519273e6f31acac58ac316499800920eb0fd0c8dba6e"
+  url "https://github.com/wata727/tflint/archive/v0.7.5.tar.gz"
+  sha256 "fbe5cd4bff55811e79adf68a0d91c2c3aa60401a93a9b5c9f5be17fb9dce1c31"
 
   depends_on "dep" => :build
   depends_on "go" => :build
