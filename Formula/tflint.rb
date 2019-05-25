e5c26b10dda54bf52f0787118e8cf5d893b96473
wata_mac <watassbass@gmail.com>
2019-05-26 01:47:13 +0900

    Bump up version to v0.8.0

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -1,8 +1,8 @@
 class Tflint < Formula
   desc "Terraform linter"
   homepage "https://github.com/wata727/tflint"
-  url "https://github.com/wata727/tflint/archive/v0.7.6.tar.gz"
-  sha256 "acc9e595888801be40fe9ba8d4ecdb5fc6a689d005ea5f7ebebfe252159a347f"
+  url "https://github.com/wata727/tflint/archive/v0.8.0.tar.gz"
+  sha256 "6d384360e7ea6dba6861b7f6628c5eccba32401d18a0eb3aa2a8c2eb5134a9d7"
 
   depends_on "dep" => :build
   depends_on "go" => :build
@@ -12,9 +12,9 @@ class Tflint < Formula
     (buildpath/"src/github.com/wata727/tflint").install buildpath.children
     cd "src/github.com/wata727/tflint" do
       system "dep", "ensure", "-vendor-only"
-      system "go", "build", "-o", "tflint"
+      system "go", "build", "-o", "dist/tflint"
 
-      bin.install "tflint"
+      bin.install "dist/tflint"
     end
   end
 
